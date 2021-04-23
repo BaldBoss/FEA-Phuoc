016969d7f9f9ab21b1401357a6a66aa7c568456d
Phuoc Phan <phanminh_phuoc@yahoo.com>
2021-04-23 20:41:34 +0700

    sua bai tap 2 qua jquery

--- baitap2/index.js
+++ baitap2/index.js
@@ -1,9 +1,6 @@
-let col = document.getElementsByClassName("collapsible");
-for(var i = 0; i < col.length; i++)
-{
-    col[i].addEventListener("click", function(){
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        content.classList.toggle("hidden")
-    })
-}
+$(document).ready(function(){
+    $(".collapsible").click(function(){
+        $(this).next().toggle();
+        $(this).toggleClass("active")
+    });
+})
